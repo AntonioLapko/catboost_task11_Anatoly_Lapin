d61bf713c3f6d4c7dc57e5e2db53af19453691f6
AntonioLapko <137512902+AntonioLapko@users.noreply.github.com>
2023-09-11 12:02:45 +0400

    1

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ student_scores = pd.DataFrame (grant, columns = ['subject1', 'subject2', 'subjec
 for i in range (2000): laboratory_work_list.insert (i, np.random.choice(laboratory_work))
 student_scores['laboratory_work'] = laboratory_work_list
 print(student_scores)
+
+print(laboratory_work)
